cc9a26f421e776cb339a3aec14f0380a50a5bb16
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2023-12-22 15:45:05 -0300

    FABRIK IK

--- Source/OnlineShooterCpp/Character/BlasterCharacter.cpp
+++ Source/OnlineShooterCpp/Character/BlasterCharacter.cpp
@@ -241,3 +241,9 @@ bool ABlasterCharacter::IsAiming()
 {
 	return (Combat && Combat->bAiming);
 }
+
+AWeapon* ABlasterCharacter::GetEquippedWeapon()
+{
+	if (Combat == nullptr) return nullptr;
+	return Combat->EquippedWeapon;
+}
