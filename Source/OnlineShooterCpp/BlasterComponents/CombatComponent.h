c8d93f16d598842f54e8cbc8c6667d2bfa19125b
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2023-12-27 10:08:05 -0300

    Replicating the Hit Target

--- Source/OnlineShooterCpp/BlasterComponents/CombatComponent.h
+++ Source/OnlineShooterCpp/BlasterComponents/CombatComponent.h
@@ -32,9 +32,9 @@ protected:
 	void FireButtonPressed(bool bPressed);
 
 	UFUNCTION(Server, Reliable)
-	void ServerFire();
+	void ServerFire(const FVector_NetQuantize& TraceHitTarget);
 	UFUNCTION(NetMulticast, Reliable)
-	void MulticastFire();
+	void MulticastFire(const FVector_NetQuantize& TraceHitTarget);
 
 	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
 
@@ -54,7 +54,5 @@ private:
 	float AimWalkSpeed;
 
 	bool bFireButtonPressed;
-
-	FVector HitTarget;
 		
 };
