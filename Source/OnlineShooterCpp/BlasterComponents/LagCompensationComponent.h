6300da5489acd98e4fb9cca83312fe19ff869329
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-04-27 11:53:54 -0300

    Rewinding Time

--- Source/OnlineShooterCpp/BlasterComponents/LagCompensationComponent.h
+++ Source/OnlineShooterCpp/BlasterComponents/LagCompensationComponent.h
@@ -40,7 +40,14 @@ public:
 	ULagCompensationComponent();
 	friend class ABlasterCharacter;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	void ShowFramePackage(const FFramePackage& Package, const FColor Color);
+	void ShowFramePackage(const FFramePackage& Package, const FColor Color); 
+	void ServerSideRewind(
+		class ABlasterCharacter* HitCharacter, 
+		const FVector_NetQuantize& TraceStart, 
+		const FVector_NetQuantize& HitLocation, 
+		float HitTime
+	);
+
 
 protected:
 	virtual void BeginPlay() override;
